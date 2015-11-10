481bf6a7375a777bd3317d926b546014c96dbfe8
Emerson <emer-martins@hotmail.com>
2015-11-10 10:53:57 -0200

    Create shifter.vhd

---
+++ VHDL/shifter.vhd
@@ -0,0 +1,21 @@
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+use IEEE.STD_LOGIC_UNSIGNED.all;
+use IEEE.STD_LOGIC_ARITH.all;
+
+entity shifter is
+port(
+	entrada : in STD_LOGIC_VECTOR(0 to 31);
+	saida : out STD_LOGIC_VECTOR(0 to 31)
+);
+end shifter;
+
+architecture arch of shifter is
+begin
+
+process
+begin
+saida <= entrada(0 to 29)&"00";
+
+end process;
+end arch;
